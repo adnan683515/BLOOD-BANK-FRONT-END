ae816c9b3639b3a8b21513de267369b282431c9c
adnan683515 <golamfaruk680@gmail.com>
2025-03-28 02:44:12 +0600

    update

--- all_request.js
+++ all_request.js
@@ -6,14 +6,77 @@
 
 async function loadAllRequest() {
     const donate_id = getLocalStorageItem('donate_id')
-    console.log(donate_id)
-    if(!donate_id){
+    if (!donate_id) {
         return
     }
     try {
         const allRequestFetch = await fetch(`https://blood-bank-backend-beta.vercel.app/requestOfuser/${donate_id}/`)
         const allData = await allRequestFetch.json()
-        console.log(allData)
+
+
+
+
+
+        const parent = document.getElementById('all-request-container')
+        allData.forEach(element => {
+
+            async function LoadUserDAta(id) {
+
+                try {
+                    const userinfo = await fetch(`https://blood-bank-backend-beta.vercel.app/DetailsUserView/${id}/`)
+                    const informatonData = await userinfo.json()
+                    
+
+                    const tr = document.createElement('tr')
+
+                    tr.innerHTML = `
+            
+            
+    
+                            <th>
+                                <label>
+                                    <input value="f" type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div class="flex items-center gap-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle h-12 w-12">
+                                            <img src="${informatonData?.picture}"
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">${element?.user?.username}</div>
+                                        <div class="text-sm opacity-50">United States</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Zemlak, Daniel and Leannon
+                                <br />
+                                <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                            </td>
+                            <td>Purple</td>
+                            <th>
+                                <button class="btn btn-ghost btn-xs">details</button>
+                            </th>
+                
+            
+            
+                        `
+                        parent.appendChild(tr)
+                }
+                catch {
+                    console.log("all request. js LoaduserData problem")
+                }
+
+            }
+            LoadUserDAta(element?.user?.id)
+            console.log(element)
+
+
+        });
     }
     catch {
         console.log('all_request.js file load all request load problem')
